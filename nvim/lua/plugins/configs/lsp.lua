cb888724baa359340674295be1126e88651fd387
Yauhen Karatkou <evgn.korotkov@gmail.com>
2025-03-06 10:22:40 +0100

    Better lsp

--- nvim/lua/plugins/configs/lsp.lua
+++ nvim/lua/plugins/configs/lsp.lua
@@ -16,6 +16,7 @@ return {
           map('n', 'gd', vim.lsp.buf.definition, opts)
           map('n', 'gD', vim.lsp.buf.declaration, opts)
           map('n', 'gI', vim.lsp.buf.implementation, opts)
+          map('n', 'gr', vim.lsp.buf.references, opts)
           map('n', 'K', vim.lsp.buf.hover, opts)
           map('n', '<C-k>', vim.lsp.buf.signature_help, opts)
           map("n", "<leader>vv", function() vim.lsp.buf.format { async = true } end, opts)
@@ -25,6 +26,58 @@ return {
           map('n', '<leader>to', vim.diagnostic.setqflist)
           map('n', '<leader>tl', vim.diagnostic.open_float)
           map({ 'n', 'v' }, '<leader>ta', vim.lsp.buf.code_action, opts)
+
+          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
+          ---@param client vim.lsp.Client
+          ---@param method vim.lsp.protocol.Method
+          ---@param bufnr? integer some lsp support methods only in specific files
+          ---@return boolean
+          local function client_supports_method(client, method, bufnr)
+            if vim.fn.has 'nvim-0.11' == 1 then
+              return client:supports_method(method, bufnr)
+            else
+              return client.supports_method(method, { bufnr = bufnr })
+            end
+          end
+
+          -- The following two autocommands are used to highlight references of the
+          -- word under your cursor when your cursor rests there for a little while.
+          --    See `:help CursorHold` for information about when this is executed
+          --
+          -- When you move your cursor, the highlights will be cleared (the second autocommand).
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+
+          -- The following code creates a keymap to toggle inlay hints in your
+          -- code, if the language server you are using supports them
+          --
+          -- This may be unwanted, since they displace some of your code
+          if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            map('n', '<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, opts)
+          end
         end
       })
 
@@ -113,6 +166,7 @@ return {
 
         ruby_lsp = {
           filetypes = { 'ruby', 'eruby', 'slim' },
+          cmd = { 'mise', 'exec', '--', 'ruby-lsp' }
         },
 
         yamlls = {
