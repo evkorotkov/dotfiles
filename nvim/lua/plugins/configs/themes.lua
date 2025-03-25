fb32b023ae6ee1eea8ced7f20ad74012167508aa
Yauhen Karatkou <evgn.korotkov@gmail.com>
2025-03-25 20:05:47 +0100

    Switch to nightfox color scheme

--- nvim/lua/plugins/configs/themes.lua
+++ nvim/lua/plugins/configs/themes.lua
@@ -17,19 +17,37 @@ return {
           })
         end,
       },
+      {
+        "EdenEast/nightfox.nvim",
+        config = function()
+          require('nightfox').setup({
+            groups = {
+              all = {
+                ["@string.special.symbol.ruby"] = { fg = "palette.green" },
+                ["@variable.member.ruby"] = { fg = "palette.orange" },
+                ["rubyTodo"] = { fg = "palette.comment", bg = "bg4" }
+              }
+            },
+          })
+        end,
+      }
     },
     init = function()
       local function is_dark_mode()
         local handle = io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null")
+        if not handle then
+          return false
+        end
+
         local result = handle:read("*a")
         handle:close()
         return result:match("Dark") ~= nil
       end
 
       if is_dark_mode() then
-        vim.cmd("colorscheme rose-pine-moon")
+        vim.cmd("colorscheme duskfox")
       else
-        vim.cmd("colorscheme rose-pine-dawn")
+        vim.cmd("colorscheme dawnfox")
       end
     end,
     config = function()
@@ -38,14 +56,14 @@ return {
         update_interval = 1000,
         set_dark_mode = function()
           vim.api.nvim_set_option_value('background', 'dark', {})
-          vim.cmd("colorscheme rose-pine-moon")
+          vim.cmd("colorscheme duskfox")
 
           -- https://github.com/neovim/neovim/issues/23590
           vim.cmd('hi! link CurSearch Search')
         end,
         set_light_mode = function()
           vim.api.nvim_set_option_value('background', 'light', {})
-          vim.cmd("colorscheme rose-pine-dawn")
+          vim.cmd("colorscheme dawnfox")
 
           -- https://github.com/neovim/neovim/issues/23590
           vim.cmd('hi! link CurSearch Search')
